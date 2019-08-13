e3500deca75a1d299f3147292c0ecc7c755b4b57
anithapaul1234 <anithapaulspyro@gmail.com>
2019-08-13 15:02:13 +0530

    custommodulesaddsubdivmult

--- node/addnum1.js
+++ node/addnum1.js
@@ -1,5 +1,23 @@
-const op=require('./operations')
+const op1=require('./operations')
 var a=5;
 var b=8;
-var c=op.add(a,b)
-console.log(c);
+var c=op1.add(a,b)
+console.log("add:"+c);
+
+const op2=require('./operations')
+var a=10;
+var b=8;
+var c=op2.sub(a,b)
+console.log("sub:"+c);
+
+const op3=require('./operations')
+var a=5;
+var b=8;
+var c=op3.mult(a,b)
+console.log("mult:"+c);
+
+const op4=require('./operations')
+var a=16;
+var b=8;
+var c=op4.div(a,b)
+console.log("div:"+c);
--- node/operations.js
+++ node/operations.js
@@ -2,3 +2,15 @@ module.exports.add=(a,b)=>{
     var c=a+b
     return c
 }
+module.exports.sub=(a,b)=>{
+    var c=a-b
+    return c
+}
+module.exports.mult=(a,b)=>{
+    var c=a*b
+    return c
+}
+module.exports.div=(a,b)=>{
+    var c=a/b
+    return c
+}
